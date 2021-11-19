5c346ca622e511d21ed72166cb4010f9e92b6ec0
Aarón Cuillas <aaroncutillas@gmail.com>
2021-11-19 23:28:27 +0100

    commit inicial

---
+++ src/proyecto_futuro/Proyecto_futuro.java
@@ -0,0 +1,26 @@
+
+package proyecto_futuro;
+
+public class Proyecto_futuro {
+
+    public static void main(String[] args) { 
+        String nombre=args[0];
+        String modulos=args[1];
+        String horasSemana=args[2];
+
+        
+        System.out.println("Estudiante " + nombre + " : ");
+        System.out.println(veredicto(modulos,horasSemana));
+    }
+    static String veredicto(String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+}
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+}
+            return "Ideal. Trabajas los contenidos en casa.";
+        
+    }
+    
+}
